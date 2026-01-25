80aeade48a6a0df3b03b7de210386b654d06eeac
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2026-01-26 03:07:14 +0700

    feat: update userdetail service

--- src/main/java/com/tetgift/service/UserService.java
+++ src/main/java/com/tetgift/service/UserService.java
@@ -1,4 +1,14 @@
 package com.tetgift.service;
 
+import com.tetgift.model.Users;
+import jakarta.mail.MessagingException;
+import org.springframework.data.domain.Page;
+
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
+
 public interface UserService {
+
+    Users findByUsername(String username);
+
 }
--- src/main/java/com/tetgift/service/impl/UserServiceImpl.java
+++ src/main/java/com/tetgift/service/impl/UserServiceImpl.java
@@ -1,4 +1,19 @@
 package com.tetgift.service.impl;
 
-public class UserServiceImpl {
+import com.tetgift.exception.ResourceNotFoundException;
+import com.tetgift.model.Users;
+import com.tetgift.repository.jpa.UserRepository;
+import com.tetgift.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public Users findByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
 }
