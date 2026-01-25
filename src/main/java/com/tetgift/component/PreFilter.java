7de3ed2e7cd58bf357145340bde035ad79638812
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2026-01-26 03:13:24 +0700

    feat: security config + prefilter

--- src/main/java/com/tetgift/component/PreFilter.java
+++ src/main/java/com/tetgift/component/PreFilter.java
@@ -1,10 +1,19 @@
 package com.tetgift.component;
 
+import com.tetgift.enums.TokenType;
+import com.tetgift.service.JwtService;
+import com.tetgift.service.impl.UserDetailsServiceImpl;
+import io.micrometer.common.util.StringUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,8 +22,32 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class PreFilter extends OncePerRequestFilter {
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    private final JwtService jwtService;
+    private final UserDetailsServiceImpl userDetailService;
 
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authorization = request.getHeader("Authorization");
+        if (StringUtils.isBlank(authorization) || !authorization.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        final String token = authorization.substring(7);
+        final String username = jwtService.extractUsername(token, TokenType.ACCESS);
+        if (StringUtils.isNotEmpty(username) && SecurityContextHolder.getContext().getAuthentication() == null) {
+            var userDetails = userDetailService.loadUserByUsername(username);
+            if (jwtService.isTokenValid(token, userDetails, TokenType.ACCESS)) {
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                securityContext.setAuthentication(authToken);
+                SecurityContextHolder.setContext(securityContext);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 }
