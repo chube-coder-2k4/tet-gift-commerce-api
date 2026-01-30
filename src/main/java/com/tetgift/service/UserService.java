f232ba8d51587ce3e315b3c81cdb4274c11d3f1c
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2026-01-30 23:58:42 +0700

    apis for user

--- src/main/java/com/tetgift/service/UserService.java
+++ src/main/java/com/tetgift/service/UserService.java
@@ -1,9 +1,24 @@
 package com.tetgift.service;
 
+import com.tetgift.dto.request.UserRequest;
+import com.tetgift.dto.request.UserUpdateRequest;
+import com.tetgift.dto.response.PageResponse;
 import com.tetgift.model.Users;
+import jakarta.mail.MessagingException;
+
+import java.io.UnsupportedEncodingException;
 
 public interface UserService {
 
     Users findByUsername(String username);
 
+    Long saveUser(UserRequest request) throws MessagingException, UnsupportedEncodingException;
+
+    Long updateUser(Long userId, UserUpdateRequest request);
+
+    void deleteUser(Long userId);
+
+    Users findById(Long userId);
+
+    PageResponse<Users> getAllUsers(int page, int size, String sortBy, String sortDir);
 }
