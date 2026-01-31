3352fc9e0c3e722ccb23eef3af9b31a5995e8c2e
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2026-01-31 19:35:21 +0700

    handle google

--- src/main/java/com/tetgift/component/OAuth2SuccessHandler.java
+++ src/main/java/com/tetgift/component/OAuth2SuccessHandler.java
@@ -1,67 +1,103 @@
 package com.tetgift.component;
 
+import com.tetgift.model.Role;
 import com.tetgift.model.Users;
+import com.tetgift.repository.jpa.RoleRepository;
 import com.tetgift.repository.jpa.UserRepository;
 import com.tetgift.service.JwtService;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.Optional;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
     private final UserRepository userRepository;
     private final JwtService jwtService;
+    private final RoleRepository roleRepository;
 
+    @Value("${app.frontend-url:http://localhost:3000}")
+    private String frontendUrl;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,Authentication authentication)
-            throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication)
+            throws IOException {
 
-        log.info("OAuth2 authentication successful for user: {}", authentication.getName());
+        try {
+            OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+            String email = oAuth2User.getAttribute("email");
 
-        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
-        String registrationId = oauthToken.getAuthorizedClientRegistrationId();
-        Map<String, Object> attributes = oauthToken.getPrincipal().getAttributes();
-        String username = (String) attributes.get("preferred_username");
-        String email = (String) attributes.get("email");
-        String firstName = (String) attributes.get("given_name");
-        String lastName = (String) attributes.get("family_name");
 
-        Optional<Users> userOpt = userRepository.findByEmail(email);
-        Users user = null;
-        if (userOpt.isPresent()) {
-            user = userOpt.get();
-        } else {
-            user = Users.builder()
-                    .username(username)
-                    .email(email)
-                    .fullName(firstName + " " + lastName)
-                    .build();
-            userRepository.save(user);
+            if (email == null || email.isEmpty()) {
+                redirectToError(request, response, "email_missing");
+                return;
+            }
+            Thread.sleep(100);
+
+            Users user = userRepository.findByEmail(email)
+                    .orElseGet(() -> {
+                        return createUserFromOAuth2(oAuth2User);
+                    });
+
+            String jwtToken = jwtService.generateAccessToken(user);
+
+            Cookie cookie = new Cookie("JWT_TOKEN", jwtToken);
+            cookie.setHttpOnly(true);
+            cookie.setSecure(false);
+            cookie.setPath("/");
+            cookie.setMaxAge(24 * 60 * 60);
+            response.addCookie(cookie);
+
+            String targetUrl = UriComponentsBuilder
+                    .fromUriString(frontendUrl + "/oauth2/redirect")
+                    .queryParam("token", jwtToken)
+                    .queryParam("email", email)
+                    .build()
+                    .toUriString();
+
+            getRedirectStrategy().sendRedirect(request, response, targetUrl);
+        } catch (Exception e) {
+            redirectToError(request, response, "server_error");
         }
-        String jwtToken = jwtService.generateAccessToken(user);
-        Cookie cookie = new Cookie("JWT_TOKEN", jwtToken);
-        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(24 * 60 * 60);
-        response.addCookie(cookie);
-
-
-        super.onAuthenticationSuccess(request, response, authentication);
     }
 
+    private Users createUserFromOAuth2(OAuth2User oAuth2User) {
+        String email = oAuth2User.getAttribute("email");
+        String name = oAuth2User.getAttribute("name");
+        String givenName = oAuth2User.getAttribute("given_name");
+        String familyName = oAuth2User.getAttribute("family_name");
+        String fullName = name != null ? name :
+                (givenName != null && familyName != null ?
+                        givenName + " " + familyName : email);
+        Set<Role> role = roleRepository.findByName("USER");
+        Users newUser = Users.builder()
+                .email(email)
+                .fullName(fullName)
+                .username(email.split("@")[0])
+                .roles(role)
+                .build();
+        return userRepository.save(newUser);
+    }
 
+    private void redirectToError(HttpServletRequest request,
+                                 HttpServletResponse response,
+                                 String errorCode) throws IOException {
+        String errorUrl = frontendUrl + "/login?error=" + errorCode;
+        getRedirectStrategy().sendRedirect(request, response, errorUrl);
+    }
 }
