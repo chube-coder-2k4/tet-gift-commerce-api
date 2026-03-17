c814a4bf54fc02b8aae67ec693f16c21bc7bfdba
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2026-03-17 18:43:25 +0700

    fix: format time to vnpay

--- src/main/java/com/tetgift/util/VNPayUtil.java
+++ src/main/java/com/tetgift/util/VNPayUtil.java
@@ -29,8 +29,9 @@ public class VNPayUtil {
         vnp_Params.put("vnp_ReturnUrl", config.getReturnUrl());
         vnp_Params.put("vnp_IpAddr", "127.0.0.1");
 
-        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        formatter.setTimeZone(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         vnp_Params.put("vnp_CreateDate", formatter.format(cld.getTime()));
 
         cld.add(Calendar.MINUTE, 15);
